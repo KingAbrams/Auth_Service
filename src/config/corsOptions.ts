8a747abb93d00e8f889cea17e42438fd6001b5d8
hannael_abrams <hannaelabrams@gmail.com>
2024-08-05 20:48:14 +0200

    add url allowed in Cors

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -1,5 +1,21 @@
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://household-management-theta.vercel.app/",
+];
+
+const originHandler = (
+  origin: string | undefined,
+  callback: (error: Error | null, allowed?: boolean) => void
+) => {
+  if (!origin || allowedOrigins.includes(origin)) {
+    callback(null, true);
+  } else {
+    callback(new Error("Not allowed by CORS"));
+  }
+};
+
 export const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: originHandler,
   credentials: true,
   sameSite: "None",
 };
